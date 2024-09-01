afaf28047ab0ef763996b161ce5583a9219ba67e
Amir Katorza <amir.katorza@gmail.com>
2024-09-01 14:17:11 +0300

    Restructure the project, added pages module

--- reflex_gpt/reflex_gpt.py
+++ reflex_gpt/reflex_gpt.py
@@ -2,52 +2,8 @@
 
 import reflex as rx
 
-from rxconfig import config
-
-from . import ui
-
-class State(rx.State):
-    """The app state."""
-
-    ...
-
-def about_us() -> rx.Component:
-    # About us Page
-    return ui.base_layout(
-        rx.color_mode.button(position="top-right"),
-        rx.vstack(
-            rx.heading("Welcome to Reflex About!", size="9"),
-            spacing="5",
-            justify="center",
-            min_height="85vh",
-        ),
-        rx.logo(),
-    )
-
-def home_page() -> rx.Component:
-    # Welcome Page (Index)
-    return ui.base_layout(
-        rx.color_mode.button(position="top-right"),
-        rx.vstack(
-            rx.heading("Welcome to Reflex GPT!", size="9"),
-            rx.text(
-                "Get started by editing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="5",
-            ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
-            ),
-            spacing="5",
-            justify="center",
-            min_height="85vh",
-        ),
-        rx.logo(),
-    )
-
+from . import pages
 
 app = rx.App()
-app.add_page(home_page, route='/')
-app.add_page(about_us, route='/about')
+app.add_page(pages.home_page(), route='/')
+app.add_page(pages.about_us_page(), route='/about')
