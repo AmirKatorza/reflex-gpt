9f387bdb10e4f18aa61b7db4434aa66758cebc10
Amir Katorza <amir.katorza@gmail.com>
2024-09-02 12:38:23 +0300

    Added storing message logic

--- reflex_gpt/chat/page.py
+++ reflex_gpt/chat/page.py
@@ -1,15 +1,20 @@
 import reflex as rx
-
 from reflex_gpt import ui
-
 from .form import chat_form
+from .state import ChatMessage, ChatState
+
+def message_box(chat_message: ChatMessage):
+    return rx.box(rx.text(chat_message.message))
 
 
 def chat_page():
-
     return ui.base_layout(
         rx.vstack(
             rx.heading("Chat Here", size="9"),
+            rx.box(
+                rx.foreach(ChatState.messages, message_box),
+                width = '100%'
+            ),
             chat_form(),
             spacing="5",
             justify="center",
